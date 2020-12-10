1273b5f501bd5042c035ba110c49d865905b84c5
userdw <insidetrainingie@gmail.com>
2020-12-10 10:18:46 +0700

    Add files via upload

---
+++ code/1. Grove - Tilt Switch/Tilt_Detector_Alarm/Tilt_Detector_Alarm.ino
@@ -0,0 +1,33 @@
+/*===============TILT DETECTOR ALARM==============
+  Dibuat Oleh Tim Digiware
+  ==========================================
+  Konfigurasi Pin:
+  |-----------------------------------------|
+  |                     | Arduino Base HAT  |
+  |-----------------------------------------|
+  |Grove - Buzzer       |         D3        |
+  |Grove - Tilt Switch  |         D2        |
+  |-----------------------------------------|
+*/
+
+int tilt = 2;   
+int buzzer = 3;  
+
+void setup() {
+ pinMode(tilt,INPUT_PULLUP);
+ pinMode(buzzer,OUTPUT);
+}
+
+void loop() {
+  if(digitalRead(tilt) == HIGH)
+  {
+    digitalWrite(buzzer, HIGH);
+  }
+
+  else
+  {
+    digitalWrite(buzzer, LOW);
+    
+  }
+
+}
